b5af3467e1fc794c3a20fd55b434618effcd82e1
IShix-g <49128115+IShix-g@users.noreply.github.com>
2026-02-16 14:28:14 +0900

    feat: add GitHub Pages deployment workflow and initial Docusaurus setup

---
+++ website/docusaurus.config.ts
@@ -0,0 +1,104 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const organizationName = 'IShix-g';
+const projectName = 'Logify-Unity-Docs';
+
+const config: Config = {
+  title: 'Logify-Unity',
+  tagline: 'Real-time in-game debug monitor for faster development.',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://ishix-g.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/${projectName}/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: organizationName, // Usually your GitHub org/user name.
+  projectName: projectName, // Usually your repo name.
+  // GitHub Pages is sensitive to trailing slashes, so it's safer to set this explicitly
+  trailingSlash: false,
+  
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ja'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          sidebarCollapsed: false,
+          sidebarCollapsible: false,
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Logify-Unity',
+      logo: {
+        alt: 'Logify Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://assetstore.unity.com/packages/slug/360718',
+          label: 'Store',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/IShix-g/Logify-Unity-Docs/discussions',
+          label: 'Discussions',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/IShix-g/Logify-Unity-Docs/issues',
+          label: 'Issues',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      copyright: `© ${new Date().getFullYear()} ${organizationName}.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['csharp']
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
